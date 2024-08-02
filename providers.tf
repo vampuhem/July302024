0574386118b42ed7d8b4cb8b3bcbcdf34f444526
vampuhem <65427410+vampuhem@users.noreply.github.com>
2024-08-02 22:56:09 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.113.0"
+      version = "3.111.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
