20d388bb541148b287b4beaf777b8f64b865beae
vampuhem <65427410+vampuhem@users.noreply.github.com>
2024-08-07 00:46:47 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.114.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.rg_location
